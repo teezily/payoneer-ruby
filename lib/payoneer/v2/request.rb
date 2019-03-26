518043a8b7df1b3c5fa25962e19e565c756a3c83
zacviandier <zacviandier@gmail.com>
2019-03-26 18:40:33 +0800

    V2 request

---
+++ lib/payoneer/v2/request.rb
@@ -0,0 +1,48 @@
+module Payoneer
+  module V2
+    class Request
+      def self.post(path, params = {})
+        call do
+          RestClient.post(
+            endpoint(path),
+            params.to_json,
+            headers
+          )
+        end
+      end
+
+      def self.get(path)
+        call do |response|
+          RestClient.get(
+            endpoint(path),
+            headers
+          )
+        end
+      end
+
+      def self.call(&block)
+        response = yield
+
+        fail Errors::UnexpectedResponseError.new(response.code, response.body) unless response.code == 200
+
+        JSON.parse(response.body)
+      end
+
+      def self.basic_auth
+        base = [
+          Payoneer.configuration.partner_username,
+         Payoneer.configuration.partner_api_password
+        ].join(':')
+        Base64.encode64("Basic#{base}")
+      end
+
+      def self.headers
+        { content_type: :json, accept: :json, authorization: basic_auth }
+      end
+
+      def self.endpoint(path)
+        URI.join(Payoneer.configuration.api_url_v2, path).to_s
+      end
+    end
+  end
+end
---
+++ spec/payoneer/v2/request_spec.rb
@@ -0,0 +1,111 @@
+require 'spec_helper'
+
+describe Payoneer::V2::Request do
+  let(:username) { 'username' }
+  let(:password) { 'password' }
+  let(:basic_auth) { Base64.encode64("Basic#{username}:#{password}") }
+
+  before do
+    Payoneer.configure do |config|
+      config.partner_username = username
+      config.partner_api_password = password
+      config.partner_id = 1
+    end
+  end
+
+  shared_examples 'reponse_unsuccessful' do |http_method|
+    context 'when the response is unsuccessful' do
+      before do
+        allow(RestClient).to receive(http_method) { double(code: 500, body: '') }
+      end
+
+      it 'raises and UnexpectedResponseError if a response code other than 200 is returned' do
+        expect { action }.to raise_error(Payoneer::Errors::UnexpectedResponseError)
+      end
+    end
+  end
+
+  describe '.post' do
+    subject(:action) { described_class.post(path, params) }
+
+    let(:path) { 'charges' }
+    let(:url) { "#{Payoneer.configuration.api_url_v2}#{path}" }
+    let(:params) do
+      {
+        test: 'test'
+      }
+    end
+
+    include_examples 'reponse_unsuccessful', :post
+
+    context 'when the response is successful' do
+      let(:response) do
+        {
+          "audit_id"=> 4031733,
+          "code"=> 0,
+          "description"=> "Success"
+        }
+      end
+      let(:stub) do
+        stub_request(:post, url).
+          with(
+            body: params.to_json,
+            headers: {
+              'Accept' => 'application/json',
+              'Authorization' => basic_auth.chomp,
+              'Content-Type' => 'application/json'
+            }
+        ).
+        to_return(status: 200, body: response.to_json)
+      end
+
+      before do
+        stub
+      end
+
+      it 'returns json response' do
+        expect(action).to eq(response)
+        expect(stub).to have_been_requested
+      end
+    end
+  end
+
+  describe '.get' do
+    subject(:action) { described_class.get(path) }
+
+    let(:path) { 'charges' }
+    let(:url) { "#{Payoneer.configuration.api_url_v2}#{path}" }
+
+    include_examples 'reponse_unsuccessful', :get
+
+    context 'when the response is successful' do
+      let(:response) do
+        {
+          "audit_id"=> 4031733,
+          "code"=> 0,
+          "description"=> "Success"
+        }
+      end
+      let(:stub) do
+        stub_request(:get, url).
+          with(
+            headers: {
+              'Accept' => 'application/json',
+              'Authorization' => basic_auth.chomp,
+              'Content-Type' => 'application/json'
+            }
+        ).
+        to_return(status: 200, body: response.to_json)
+      end
+
+      before do
+        stub
+      end
+
+      it 'returns json response' do
+        expect(action).to eq(response)
+        expect(stub).to have_been_requested
+      end
+    end
+  end
+end
